2412d9799e807cdc76db937b33f8155358ada091
madityaar <bftiga@gmail.com>
2018-09-06 17:59:23 +0700

    fix 5000 string

--- Tugas1/client.py
+++ Tugas1/client.py
@@ -1,13 +1,14 @@
+# encoding=utf-8
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host ="127.0.0.1"
-port =8000
+port =8002
 s.connect((host,port))
 
 def ts(str):
    x = input("enter message plz: ")
-   s.send(x.encode()) 
+   s.send(x.encode())
    data = ''
    data = s.recv(1024).decode()
    print (data)
@@ -16,3 +17,4 @@ while 2:
    ts(s)
 
 s.close ()
+exit()
--- Tugas1/server.py
+++ Tugas1/server.py
@@ -1,23 +1,27 @@
+# encoding=utf-8
+
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "127.0.0.1"
-port = 8000
+port = 8002
 data = ""
 print (host)
 print (port)
-serversocket.bind(("127.0.0.1", 8000))
+serversocket.bind(("127.0.0.1", 8002))
 
 serversocket.listen(5)
 print ('server started and listening')
 while 1:
     (clientsocket, address) = serversocket.accept()
     print ("connection found!")
-    while data!="exit":
-        data = clientsocket.recv(1024).decode("utf-8")
+    while data != "exit":
+        data = clientsocket.recv(5120).decode("utf-8")
         print ("string from client: ",data)
-        if data!="exit"
+        if data != "exit":
             r="Receieved!"
-        else
+        else:
             r="server closing"
         clientsocket.send(r.encode())
+
+exit()
